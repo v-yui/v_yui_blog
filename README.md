9a62c4e51bd595e6e3b983ba12d606fc229ea178
v-yui <1356674019@qq.com>
2020-04-29 19:08:40 +0800

    新建项目及Home

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+#### 2020.4.29
+1. 重新建立项目并配置；
+2. 整理文件夹并写一点home页；
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="HomeContainer">
+      <h1>This is yui's blog ;).</h1>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+.HomeContainer {
+    width: 100%;
+    height: 100%;
+    background-color: #6589d7;
+    position: absolute;
+    text-align: center;
+    color: white;
+}
+</style>
