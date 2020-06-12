9da62d3373d0978c54c7dfbeda28cfce30f8a634
v-yui <1356674019@qq.com>
2020-06-12 10:59:15 +0800

    放置一下

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="HomeContainer">
-      <h1>This is yui's blog ;).</h1>
+    <div class="middle">
+      <h1>This is yui's blog ;)</h1>
+      <h1 id="flower">❀</h1>
+    </div>
   </div>
 </template>
 <script>
-export default {
-}
+export default {}
 </script>
-<style scoped>
+<style lang="less" scoped>
 .HomeContainer {
-    width: 100%;
-    height: 100%;
-    background-color: #6589d7;
+  width: 100%;
+  height: 100%;
+  background-color: #6589d7;
+  position: absolute;
+  text-align: center;
+  color: white;
+
+ .middle {
+    width: 50%;
+    background-color: white;
+    border-radius: 5px;
+    box-shadow: 5px 8px 10px rgb(50, 68, 116);
+    padding: 3px;
     position: absolute;
-    text-align: center;
-    color: white;
+    top: 50px;
+    left: 30px;
+    color: #dcdcdc;
+    transition: all 0.5s;
+
+    h1 {
+      float: left;
+    }
+    #flower {
+      transition: transform 1s;
+      transform-origin: 50% 50%;
+    }
+    #flower:hover {
+      transform: rotate(360deg);
+    }
+  }
+  .middle:hover {
+    color: #6589d7;
+    box-shadow: 5px 8px 10px rgb(98, 118, 172);
+  }
 }
 </style>
